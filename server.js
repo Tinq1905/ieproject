b3e264cef72d4f4af9881a333c93132b5a28bbb9
Tinq1905 <Tinq1905@yahoo.com>
2017-03-23 16:00:08 +1100

    differenciate apitest and api

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/test",function(req,res){
 	res.send("Test - Techsquad");
 })
 
-app.get("/apitest", function(req, res){
+app.get("/api", function(req, res){
 	var type = req.query.searchType;
 	var location = req.query.myLocation;
 	var radius = 500;
@@ -28,6 +28,31 @@ app.get("/apitest", function(req, res){
 	var turl ="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&types=food&rankby=distance&key=AIzaSyDWr-XTd2CRiUhzGgaGBIYm7_HZE09hgqg";
 	sendBack(furl,res);
 })
+
+app.get("/apitest", function(req, res){
+	var type = req.query.searchType;
+	var location = req.query.myLocation;
+	var radius = 500;
+	var gkey = "AIzaSyDWr-XTd2CRiUhzGgaGBIYm7_HZE09hgqg";
+	var furl ="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+location +"&types="+type + "&rankby=distance" + "&key="+gkey;
+	var turl ="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&types=food&rankby=distance&key=AIzaSyDWr-XTd2CRiUhzGgaGBIYm7_HZE09hgqg";
+	sendBackTest(furl,res);
+})
+
+function sendBackTest (url,res) {
+	var foo = [];
+	https.get(url, function(response) {
+		var body ="";
+		response.on('data', function(chunk) {
+			body += chunk;
+		})
+		response.on('end', function () {
+			places = JSON.parse(body);
+			var results = places.results;
+			res.send(results);
+		});
+	})
+}
 
 function sendBack (url,res) {
 	var foo = [];
@@ -43,7 +68,7 @@ function sendBack (url,res) {
 			for (i=0;i<5;i++){
 				foo.push(results[i].name);
 				foo.push(results[i].geometry.location);
-				foo.push(results[i].opening_hours.open_now);
+				foo.push(results[i].opening_hours);
 				foo.push(results[i].place_id);
 				foo.push(results[i].price_level);
 				foo.push(results[i].rating);
