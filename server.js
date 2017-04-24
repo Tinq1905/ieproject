555597ca487470484d0ce7ef1155437ce3507432
Tinq1905 <Tinq1905@yahoo.com>
2017-04-24 15:02:06 +1000

    updated key

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ function basicInfo(type,location,callback){
 	var g2key = "AIzaSyDWr-XTd2CRiUhzGgaGBIYm7_HZE09hgqg";
 	var s1key = "AIzaSyCptoojRETZJtKZCTgk7Oc29Xz0i-B6cv8";
 	var s2key = "AIzaSyAMW8Z_cdUbbVMMviRfe845JBj7xbKhRp4";
-	var purl ="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+ alocation +"&types="+atype + "&rankby=distance" + "&key="+g1key;
+	var purl ="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+ alocation +"&types="+atype + "&rankby=distance" + "&key="+g2key;
 	console.log(purl);
 	https.get(purl, function(response) {
 		var body ="";
@@ -145,7 +145,7 @@ function detailedInfo(final,callback){
         ++count;
 		var placeId = final[i].place_id;
 		//Google API address.
-		var durl = "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + placeId + "&key=" + g1key;
+		var durl = "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + placeId + "&key=" + g2key;
         function back (durl,i){
 		https.get(durl,function(response) {
 			var body ="";
